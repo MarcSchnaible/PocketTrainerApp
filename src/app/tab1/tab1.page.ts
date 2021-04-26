6f427d15241ad5b3359748f4d34ce326f172c4d8
MarcSchnaible <marcschnaible@web.de>
2021-04-26 14:07:47 +0200

    initial commit: App with home page and possible to open and close the camera

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Plugins } from "@capacitor/core"
+const { CameraPreview } = Plugins;
+import { CameraPreviewOptions } from '@capacitor-community/camera-preview';
+
+
+import '@capacitor-community/camera-preview'
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +13,24 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  image = null;
+  cameraActive = false;
+
   constructor() {}
+
+  openCamera() {
+    const cameraPreviewOptions: CameraPreviewOptions = {
+      position: 'front',
+      parent: 'cameraPreview',
+      className: 'cameraPreview'
+    };
+    CameraPreview.start(cameraPreviewOptions);
+    this.cameraActive = true;
+  }
+
+  async stopCamera() {
+    await CameraPreview.stop();
+    this.cameraActive = false;
+  }
 
 }
