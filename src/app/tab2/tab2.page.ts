aeaceb3627ad3c32b261947cfec2ab287a6ea2b3
MarcSchnaible <marcschnaible@web.de>
2021-04-26 19:11:01 +0200

    started to add Tensorflow Model PoseNet

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as posenet from '@tensorflow-models/posenet';
+import * as tf from '@tensorflow/tfjs';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit{
+
+  model = null;
 
   constructor() {}
+
+  ngOnInit() {
+    this.loadModel();
+  }
+
+  async loadModel() {
+    this.model = await posenet.load();
+  }
+
+  async detect() {
+    const pose = await this.model.estimateSinglePose();
+  }
+
+  
 
 }
